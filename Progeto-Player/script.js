db51832558c2d67feae85b8c746c217a5a8961cd
Davi Santos <davirr1220@gmail.com>
2022-10-19 10:51:46 -0300

    dando continuidade no codigo

--- Progeto-Player/script.js
+++ Progeto-Player/script.js
@@ -1,11 +1,36 @@
+  let musica = document.querySelector('audio');
 
+   //Eventos
+  document.querySelector('.botao-play').addEventListener('click',tocarMusica);
 
-let musica = document.querySelector('audio');
-let indexMusica = 0;
+  document.querySelector('.botao-pause').addEventListener('click', pausarMusica); 
 
-document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+  musica.addEventListener('timeupdate',atualizarBarra);
 
-
-
-
-
+  //Funçoes
+  function tocarMusica() {
+    musica.play()
+    document.querySelector('.botao-pause').style.display= 'block';
+    document.querySelector('.botao-play').style.display= 'none';
+   }
+     
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display= 'none';
+    document.querySelector('.botao-play').style.display= 'block';
+}
+ 
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100  )+ '%' ;
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaminutos(Math.floor(musica.currentTime));
+ }
+ function segundosParaminutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0'+ campoSegundos; 
+    }
+    return campoMinutos+':'+campoSegundos;
+ }
